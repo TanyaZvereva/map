2beba6d5a06392551652d4751855b0b9c39d27ba
Tanya Zvereva <darkmarketgirl3@yandex.ru>
2021-04-24 10:44:52 +0300

    eslint commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 export default class ErrorRepository {
   constructor() {
-    this.errors = new Map()
-    this.errors.set(1, 'Неправильный пароль')
-    this.errors.set(2, 'Неправильное имя пользователя')
+    this.errors = new Map();
+    this.errors.set(1, 'Неправильный пароль');
+    this.errors.set(2, 'Неправильное имя пользователя');
   }
+
   translate(code) {
     if (this.errors.has(code)) {
-      return this.errors.get(code)
-    } else {
-      return 'Unknown error'
+      return this.errors.get(code);
     }
+    return 'Unknown error';
   }
 }
